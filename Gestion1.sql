eded69a8f4abc6dd29ab88e878b7c330ccfb2803
ZACKstrX <zakimatozi@gmail.com>
2025-03-06 12:30:19 +0000

    insert into database

--- Gestion1.sql
+++ Gestion1.sql
@@ -13,4 +13,14 @@ Create table Employes(
 	N_dep int,  
 	FOREIGN KEY (N_dep) REFERENCES Departements(N_dep),
 	);
---------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------
+insert into Departements values(1,'informatique','tanger'),
+							   (2, 'resource_humain','tanger'),
+							   (3,'administratif','tanger'),
+							   (4,'financier','tanger');
+
+insert into Employes values (1,'Ahmed','Developpeur_Informatique',8000,1500,1),
+							(2,'Amine','Security_Officer',20000,3000,1),
+							(3,'Salah', 'Recruteur', 12000,1000,2),
+							(4,'karim', 'Comtable', 11000,2000,3),
+							(5,'jawad','Directeur_general',2000,5000,4);

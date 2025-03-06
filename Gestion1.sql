42735f0dcc6f369092fc1f13338efdf03f59d781
ZACKstrX <zakimatozi@gmail.com>
2025-03-06 13:14:32 +0000

    some selection queries

--- Gestion1.sql
+++ Gestion1.sql
@@ -16,7 +16,7 @@ Create table Employes(
 ------------------------------------------------------------------------------------------------------------------------------------------------
 insert into Departements values(1,'informatique','tanger'),
 							   (2, 'resource_humain','tanger'),
-							   (3,'administratif','tanger'),
+							   (3,'admininstratif','tanger'),
 							   (4,'financier','tanger');
 
 insert into Employes values (1,'Ahmed','Developpeur_Informatique',8000,1500,1),
@@ -24,3 +24,9 @@ insert into Employes values (1,'Ahmed','Developpeur_Informatique',8000,1500,1),
 							(3,'Salah', 'Recruteur', 12000,1000,2),
 							(4,'karim', 'Comtable', 11000,2000,3),
 							(5,'jawad','Directeur_general',2000,5000,4);
+----------------------------------------------------------------------------------------------------------------------------------------------
+select Nom_E from  Employes where COMM is not null;
+select Nom_E,PROF,SAL FROM Employes order by PROF ASC,SAL DESC;
+SELECT AVG (SAL)as Salaire_moyenne from Employes;
+Select AVG (SAL)from Employes E, Departements D where E.N_dep = D.Nom_dep and D.Nom_dep='financier';
+select N_Dep,Max(SAL)as Salaire_moyenne_Production from Employes;
